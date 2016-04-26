3bd5432afc211dffa712811ef963bf424a532759
binghammg <binghammg@gmail.com>
2016-04-25 20:57:55 -0600

    Initial Commit

---
+++ src/mgb.shoppinglist.core/Model/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mgb.shoppinglist.core
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public int ItemCategoryId { get; set; }
+        public string ItemDescription { get; set; }
+        public int Quantity { get; set; }
+        public int AddedToCart { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/mgb.shoppinglist.core/Model/ItemCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mgb.shoppinglist.core.Model
+{
+    public class ItemCategory
+    {
+        public int ItemCategoryId { get; set; }
+        public string ItemCategoryDescription { get; set; }
+    }
+}
